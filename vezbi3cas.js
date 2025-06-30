0800e21f5652320f99a1496475e06e47299281f8
Dzikle1 <ace.ace1982@yahoo.com>
2025-06-30 12:45:40 +0200

    delited 2 files

--- vezbi3cas.js
+++
@@ -1,6 +0,0 @@
-const conversions = require('./conversion.js');
-
-console.log("5 mi =", conversions.convertMilesToKm(5), "km");
-console.log("20°C =", conversions.convertCelsiusToFahrenheit(20), "°F");
-console.log("150 lbs =", conversions.convertPoundsToKilogram(150), "kg");
-console.log("6 ft =", conversions.convertFeetToMetres(6), "m");
